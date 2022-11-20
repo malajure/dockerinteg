01a98205f5e4393d3f6750dde6ba0e5f0b09209b
malajure <malajure.santosh@gmail.com>
2022-11-20 09:38:55 +0530

    first

---
+++ src/main/java/com/dockerinteg/DockerintegApplication.java
@@ -0,0 +1,16 @@
+package com.dockerinteg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerintegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerintegApplication.class, args);
+	}
+}
+
+
+
+
